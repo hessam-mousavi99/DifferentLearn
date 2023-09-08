9568bb2cffbb6c9ed2e2c60cf4b53849e15e2fb2
hessam mousavi <hessam.mousavi99@gmail.com>
2023-09-08 16:49:53 +0330

    Add UtitlityMethods

---
+++ DifferentLearn.Core/Security/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentLearn.Core.Security
+{
+    public static class PasswordHelper
+    {
+        public static string  EncodePasswordMD5(string password)
+        {
+            Byte[] originalByte;
+            Byte[] encodeByte;
+            MD5 md5;
+            md5=new MD5CryptoServiceProvider();
+            originalByte=ASCIIEncoding.Default.GetBytes(password);
+            encodeByte=md5.ComputeHash(originalByte);
+            return BitConverter.ToString(encodeByte);
+        }
+    }
+}
