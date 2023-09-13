ecb562788a64a82684e5488d20184ee10ca14026
hessam mousavi <hessam.mousavi99@gmail.com>
2023-09-13 15:09:49 +0330

    convert and resize image for show in current size and thumbnail

---
+++ DifferentLearn.Core/Security/ImageValidator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentLearn.Core.Security
+{
+    public static class ImageValidator
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+			try
+			{
+				var img = System.Drawing.Image.FromStream(file.OpenReadStream());
+				return true;
+			}
+			catch 
+			{
+				return false;
+			}
+        }
+    }
+}
