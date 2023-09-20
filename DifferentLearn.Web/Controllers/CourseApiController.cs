cf7ed9136efd72b467045a8f4d9a5ccfb868dbc3
hessam mousavi <hessam.mousavi99@gmail.com>
2023-09-20 20:08:25 +0330

    api for autocomplete search

---
+++ DifferentLearn.Web/Controllers/CourseApiController.cs
@@ -0,0 +1,35 @@
+﻿using DifferentLearn.Data.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DifferentLearn.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseApiController : ControllerBase
+    {
+        DiffLearnContext _context;
+        public CourseApiController(DiffLearnContext context)
+        {
+            _context = context;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public async Task<IActionResult> Search()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"].ToString();
+                var courseTitle=await _context.Courses.Where(c=>c.CourseTitle.Contains(term)).Select(c=>c.CourseTitle).ToListAsync();  
+                return Ok(courseTitle);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+    }
+}
